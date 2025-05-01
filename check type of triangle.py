917c6d8e8f5b1d23830067b7df6bc2adb4d5bc9e
whinssto1xv <nothuman207@gmail.com>
2025-05-01 17:20:10 +0530

    Add files via upload

---
+++ check type of triangle.py
@@ -0,0 +1,26 @@
+a=int(input("enter no"))
+b=int(input("enter no"))
+c=int(input("enter no"))
+if a+b==b+c==c+a:
+    print("equtorial triangle")
+else:
+     print("not equtorial trianglre")
+
+
+# new isosceles triangle
+a=int(input("enter no"))
+b=int(input("enter no"))
+c=int(input("enter no"))
+if a==b!=c:
+    print("isosceles triangle")
+else:
+        print("not isosceles triangle")
+
+
+# new isoscelence tringle
+b=int(input("enter no"))
+c=int(input("enter no"))
+if a!=b!=c:
+    print("scalence triangle")
+else:
+     print("not scalance triangle")
